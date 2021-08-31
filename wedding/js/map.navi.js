1f8532e661077c567d4e92d414e8027c65ba4ecc
munifico <jihun85112@empal.com>
2021-08-31 17:55:54 +0900

    navigation 및 방명록 추가

---
+++ wedding/js/map.navi.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    Kakao.init('8b80bf1dfc8174ed6e7f9308a0a9a733');
+    Kakao.isInitialized();
+
+    let kakaonaviButton = document.getElementById('kakaoRoad');
+    let kakaomapButton = document.getElementById('kakaoMap');
+});
